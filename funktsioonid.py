6c990a42e866a3ca42af62e0e239d0d1c9966da8
Arturilumäe <arturilume@ut.ee>
2025-12-10 19:47:39 +0200

    uuendus

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import json as js
+import os
 
-def aine(aine):
+def aine_exel(aine):
     df = pd.read_excel("andmed.xlsx", sheet_name=aine, header=None)  # header=None, et saaks read indekseerida
-    kõik_alampiirid = alampiir = kõik_max_punktid = max_punktid = []
+    kõik_alampiirid, alampiir, kõik_max_punktid, max_punktid = [],[],[],[]
 
     j=1
     for i in range(len(df)): # i on ridade arv
         cell = df.iat[i, 0]  # veerg A
         if cell == "UUS": #kui tuleb "UUS", siis alustan uut katekooriat
-            alampiir = dict(zip(df.iloc[j:i, 0], df.iloc[j:i, 1]))
-            max_punktid = dict(zip(df.iloc[j:i, 0], df.iloc[j:i, 2]))
+            alampiir = dict(zip(df.iloc[j:i, 0], df.iloc[j:i, 1])) #Veerg B
+            max_punktid = dict(zip(df.iloc[j:i, 0], df.iloc[j:i, 2])) #veerg C
             kõik_alampiirid.append(alampiir)
             kõik_max_punktid.append(max_punktid)
+            kõik_max_punktid.append("UUS")
+            kõik_alampiirid.append("UUS")
             j = i+1
             continue
     if j < len(df): #viimase plokki lisamine
@@ -27,24 +31,82 @@ def aine(aine):
 
     return kõik_alampiirid, kõik_max_punktid, hinded, punktid_hindeks
 
-def küsi_punktid(max_punktid):
+
+def local_save(data, location=""):
+    location = location + "Kasutaja_hinded.json"
+    if not os.path.exists(location):
+        with open(location, 'w', encoding='utf-8') as f:
+            f.write("{}")
+    with open(location, 'w', encoding='utf-8') as f:
+        js.dump(data, f, ensure_ascii=False, indent=4)
+
+def get_local_data(location=""):
+    location = location + "Kasutaja_hinded.json"
+    if os.path.exists(location) == False:
+        return None
+    with open(location, 'r', encoding='utf-8') as f:
+        data = js.load(f)
+    return data
+
+def küsi_punktid(max_punktid,andmed=None):
     grades = {}
-    #for i in max_punktid:  # Iga kategooria kohta
-    while True:
-        sisend = (f"{i}: ")
-        if sisend != "-":
-            try:
-                punkt = int(sisend)
-                if 0 <= punkt <= max_punktid[i]:
-                    grades[i] = (punkt)
-                    break
-                else:
-                    print(f"Palun sisesta punktid vahemikus 0 kuni {max_punktid[i]}.")
-            except ValueError:
-                print("Palun sisesta kehtiv arv või '-'.")
-        else:
-            grades[i] = None
+    for i in max_punktid:  # Iga kategooria kohta
+        grades[i] = []
+        if i == "UUS":
+            return grades
             break
+        elif andmed != None: #kui uuendatakse
+                print(f"Praegu on {andmed.get(i)} punkti kategoorias '{i}'.\nMax punktid on {max_punktid[i]}\nSisesta uued punktid, '-' kui ei tea veel või 'X' kui tahad lõpetada punktide sisestamise:")
+                x = 1
+                punktid=0
+                while True:
+                    sisend = input(f"{x}: ").strip()
+                    if sisend.upper() == "X":
+                        break
+                    elif sisend != "-":
+                        try:
+                            punkt = float(sisend)
+                            if 0 <= punkt:
+                                punktid+=punkt
+                                if punktid <= max_punktid[i]:
+                                    grades[i].append(punkt)
+                                    x+=1
+                                else:
+                                    print(f"Sisestatud punktid ({punkt} + {punktid}) ületavad maksimaalseid punkte {max_punktid[i]} selles kategoorias.")
+
+                            else:
+                                print(f"Palun sisesta punktid 0 või suurem.")
+                        except ValueError:
+                            print("Palun sisesta kehtiv arv, '-' või 'X'.")
+                    elif sisend == "-":
+                        grades[i].append(None)
+                        x+=1
+        elif andmed == None: #kui sisestatakse esimest korda
+            print(f"Praegu on '{i}' kategooria. Max punktid on {max_punktid[i]}.\nSisesta uued punktid, '-' kui ei tea veel või 'X' kui tahad lõpetada punktide sisestamise:")
+            x = 1
+            punktid=0
+            while True:
+                sisend = input(f"{x}: ").strip()
+                if sisend.upper() == "X":
+                    break
+                elif sisend != "-":
+                    try:
+                        punkt = float(sisend)
+                        if 0 <= punkt:
+                            punktid+=punkt
+                            if punktid <= max_punktid[i]:
+                                grades[i].append(punkt)
+                                x+=1
+                            else:
+                                print(f"Sisestatud punktid ({punkt} + {punktid}) ületavad maksimaalseid punkte {max_punktid[i]} selles kategoorias.")
+                                    
+                        else:
+                            print(f"Palun sisesta punktid 0 või suurem.")
+                    except ValueError:
+                        print("Palun sisesta kehtiv arv, '-' või 'X'.")
+                elif sisend == "-":
+                    grades[i].append(None)
+                    x+=1
     return grades
 
 def arvuta_hinne(kokku_punkte, punktid_hindeks, hinded):
@@ -57,3 +119,25 @@ def arvuta_hinne(kokku_punkte, punktid_hindeks, hinded):
             saadud_hinne = hinded[i]
             break
     return saadud_hinne
+
+"""
+andemd = {
+    "Programeerimine 1": {
+        "testid":[1, 0, 0.5, 0.25],
+        "kodutöö": [2, 1, 0.5, 3],
+        "praktikum": "-",
+        "projekt": "-",
+        "1.kontrolltöö": 19,
+        "2.kontrolltöö": "-",
+        "Eksam": "-",
+        "Lisapunktid": [1,2]
+    },
+    "AAR 1": {
+        "kontrolltööd": [3, 10, 5, "-"],
+        "eksam": "-",
+        "praktikum": 90
+    }
+}
+
+local_save(andemd)
+"""
--- main.py
+++ main.py
@@ -1,16 +1,89 @@
 import funktsioonid as fn # kus kõik funktsioonid on 
 
-grades = {}
+grades = { #ainete määramine
+    "Programeerimine 1": {},
+    "AAR 1": {},
+    "Opsys": {},
+    "KÕM 1": {},
+    "Sissejuhatus erialasse": {}
+}
 subjects = { #ainete määraine
-    "Programeerimine": "Programmeerimine 1",
+    "Programmeerimine": "Programmeerimine 1",
     "Arvuti arhitektuur ja riistvara 1": "AAR 1",
     "Operatsioonisüsteemid": "Opsys",
     "Kõrgem matemaatika 1": "KÕM 1",
     "Sissejuhatus erialasse": "Sissejuhatus erialasse"
 }
+käsud = ["Sisesta punktid", "Arvuta hinne #Pole valmis", "Salvesta andmed #Pole valmis", "Loe salvestatud andmed #Pole valmis", "Välju"]
 
 subject_list = list(subjects.keys())
-print("Vali õppeaine: ")
+
+print("Tere tulemast hinde arvutaja programmi!")
+print("Kas sa soovid oma andmed laadida eelmisest sessioonist? (jah/ei)")
+load_choice = input().strip().lower()
+if load_choice == "jah":
+    saved_data = fn.get_local_data()
+    if saved_data:
+        print("Andmed leitud ja laetud.")
+        print(saved_data)
+    else:
+        print("Eelnevaid andmeid ei leitud.")
+print()
+
+
+while True: #Põhiprogramm
+    print("Mida teha soovid?")
+    for i in käsud:
+        print(f"- {i}")
+    cmd_choice = input("Sisesta käsk: ").strip().lower()
+
+    if cmd_choice != "välju":
+        print("Mis õppeainel: ")
+        for i, subj in enumerate(subject_list, 1): #aine küsimine
+            print(f"{i}. {subj}")
+        while True:
+            choice = int(input("Sisesta õppeaine number: "))
+            if 1 <= choice <= len(subject_list):
+                subject_key = subject_list[choice - 1]
+                subject = subjects[subject_key]
+                print(f"Valisid: {subject_key}")
+                break
+            else:
+                print("Vale valik!")
+    print()
+    alampiirid, max_punktid, hinded, punktid_hindeks = fn.aine_exel(subject) # Andmed tapelist
+    match cmd_choice:
+        case "sisesta punktid":
+                if 'saved_data' in locals():
+                    print("Kas tahad uuendada oma punkte või alustada algusest? (uuenda/algus)")
+                    update_choice = input().strip().lower()
+                    if update_choice == "uuenda":
+                        # Kutsu `küsi_punktid`, edastades olemasolevad salvestatud andmed
+                        grades[subject] = fn.küsi_punktid(max_punktid[0], saved_data.get(subject))
+                    else:
+                        grades[subject] = fn.küsi_punktid(max_punktid[0])
+                else:
+                    grades[subject] = fn.küsi_punktid(max_punktid[0])
+        case "välju":
+            print("Programm lõpetab töö.")
+            print("Kas tahand andmed salvestada? (jah/ei)")
+            save_choice = input().strip().lower()
+            if save_choice == "jah":
+                fn.local_save(grades)
+                print("Andmed salvestatud.")
+            break
+
+"""
+grades = fn.küsi_punktid(max_punktid[0])  # Preagu kasutame esimest katekooriat   !!!NB HILJEM MUUTA
+
+kokku_punkte = sum(p for p in grades.values() if p is not None)
+
+saadud_hinne = fn.arvuta_hinne(kokku_punkte, punktid_hindeks, hinded)
+
+print(f"Sinu hinne ja punktid hetkel on: {saadud_hinne} ja {kokku_punkte} punkti.")
+
+
+print("Nüüd vali õppeaine: ")
 for i, subj in enumerate(subject_list, 1): #aine küsimine
     print(f"{i}. {subj}")
 
@@ -21,16 +94,6 @@ if 1 <= choice <= len(subject_list):
     print(f"Valisid: {subject_key}")
 else:
     print("Vale valik!")
-
-alampiirid, max_punktid, hinded, punktid_hindeks = fn.aine(subject) # Andmed tapelist
-print(f"Valisid õppeaine: {subject}")
-print("Sisesta oma punktid järgmiste katekooriate kohta ('-' tähendab, et puudub tulemus):")
-
-grades = fn.küsi_punktid(max_punktid[0])  # Preagu kasutame esimest katekooriat   !!!NB HILJEM MUUTA
-
-kokku_punkte = sum(p for p in grades.values() if p is not None)
-
-saadud_hinne = fn.arvuta_hinne(kokku_punkte, punktid_hindeks, hinded)
-
-print(f"Sinu hinne ja punktid hetkel on: {saadud_hinne} ja {kokku_punkte} punkti.")
-
+print()
+alampiirid, max_punktid, hinded, punktid_hindeks = fn.aine_e(subject) # Andmed tapelist
+"""
